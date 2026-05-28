85f87297804e053a0e796e34098a9beb455812cc
sentseven <sentsven@gmail.com>
2026-05-28 18:48:20 -0500

    Extract Rust sync_vendor implementation

--- rusty/src/bin/sync_vendor.rs
+++ rusty/src/bin/sync_vendor.rs
@@ -1,91 +1,105 @@
-use anyhow::{Context, Result};
-use rayon::prelude::*;
-use std::fs::{self, File};
+use anyhow::Result;
+use reqwest::blocking::Client;
+use std::fs::File;
 use std::io::{BufRead, BufReader, Write};
 use std::path::Path;
 
-const HTTPLIB_VERSION: &str = "refs/tags/v0.40.0";
-const BORDER: &str =
-    "// ----------------------------------------------------------------------------";
-
 fn main() -> Result<()> {
-    let mut vendor_files = vec![
-        ("https://github.com/nlohmann/json/releases/latest/download/json.hpp".to_string(), "vendor/nlohmann/json.hpp".to_string()),
-        ("https://github.com/nlohmann/json/releases/latest/download/json_fwd.hpp".to_string(), "vendor/nlohmann/json_fwd.hpp".to_string()),
-        ("https://raw.githubusercontent.com/nothings/stb/refs/heads/master/stb_image.h".to_string(), "vendor/stb/stb_image.h".to_string()),
-        ("https://github.com/mackron/miniaudio/raw/9634bedb5b5a2ca38c1ee7108a9358a4e233f14d/miniaudio.h".to_string(), "vendor/miniaudio/miniaudio.h".to_string()),
-        ("https://raw.githubusercontent.com/sheredom/subprocess.h/b49c56e9fe214488493021017bf3954b91c7c1f5/subprocess.h".to_string(), "vendor/sheredom/subprocess.h".to_string()),
+    let htpplib_version = "refs/tags/v0.40.0";
+
+    let vendor_files = vec![
+        (
+            "https://github.com/nlohmann/json/releases/latest/download/json.hpp".to_string(),
+            "vendor/nlohmann/json.hpp".to_string(),
+        ),
+        (
+            "https://github.com/nlohmann/json/releases/latest/download/json_fwd.hpp".to_string(),
+            "vendor/nlohmann/json_fwd.hpp".to_string(),
+        ),
+        (
+            "https://raw.githubusercontent.com/nothings/stb/refs/heads/master/stb_image.h".to_string(),
+            "vendor/stb/stb_image.h".to_string(),
+        ),
+        (
+            "https://github.com/mackron/miniaudio/raw/9634bedb5b5a2ca38c1ee7108a9358a4e233f14d/miniaudio.h".to_string(),
+            "vendor/miniaudio/miniaudio.h".to_string(),
+        ),
+        (
+            format!("https://raw.githubusercontent.com/yhirose/cpp-httplib/{}/httplib.h", htpplib_version),
+            "httplib.h".to_string(),
+        ),
+        (
+            format!("https://raw.githubusercontent.com/yhirose/cpp-httplib/{}/LICENSE", htpplib_version),
+            "vendor/cpp-httplib/LICENSE".to_string(),
+        ),
+        (
+            "https://raw.githubusercontent.com/sheredom/subprocess.h/b49c56e9fe214488493021017bf3954b91c7c1f5/subprocess.h".to_string(),
+            "vendor/sheredom/subprocess.h".to_string(),
+        ),
     ];
 
-    let httplib_h_url = format!(
-        "https://raw.githubusercontent.com/yhirose/cpp-httplib/{}/httplib.h",
-        HTTPLIB_VERSION
-    );
-    let httplib_license_url = format!(
-        "https://raw.githubusercontent.com/yhirose/cpp-httplib/{}/LICENSE",
-        HTTPLIB_VERSION
-    );
+    let client = Client::new();
 
-    vendor_files.push((httplib_h_url, "httplib.h".to_string()));
-    vendor_files.push((
-        httplib_license_url,
-        "vendor/cpp-httplib/LICENSE".to_string(),
-    ));
-
-    // Download files in parallel
-    vendor_files
-        .par_iter()
-        .try_for_each(|(url, filename)| -> Result<()> {
-            println!("downloading {} to {}", url, filename);
-            if let Some(parent) = Path::new(filename).parent() {
-                fs::create_dir_all(parent)?;
-            }
-            let response = reqwest::blocking::get(url)
-                .with_context(|| format!("Failed to download {}", url))?
-                .bytes()?;
-            fs::write(filename, response)
-                .with_context(|| format!("Failed to write {}", filename))?;
-            Ok(())
-        })?;
-
-    println!("Splitting httplib.h...");
-    split_httplib_h("httplib.h", "vendor/cpp-httplib")?;
-
-    fs::remove_file("httplib.h")?;
-
-    Ok(())
-}
-
-fn split_httplib_h(in_file: &str, out_dir: &str) -> Result<()> {
-    fs::create_dir_all(out_dir)?;
-
-    let h_out_path = format!("{}/httplib.h", out_dir);
-    let cc_out_path = format!("{}/httplib.cpp", out_dir);
-
-    let input = File::open(in_file)?;
-    let reader = BufReader::new(input);
-
-    let mut h_out = File::create(&h_out_path)?;
-    let mut cc_out = File::create(&cc_out_path)?;
-
-    writeln!(cc_out, "#include \"httplib.h\"")?;
-    writeln!(cc_out, "namespace httplib {{")?;
-
-    let mut in_implementation = false;
-
-    for line in reader.lines() {
-        let line = line?;
-        if line.contains(BORDER) {
-            in_implementation = !in_implementation;
-        } else if in_implementation {
-            writeln!(cc_out, "{}", line.replace("inline ", ""))?;
-        } else {
-            writeln!(h_out, "{}", line)?;
+    for (url, filename) in vendor_files {
+        println!("downloading {} to {}", url, filename);
+        let path = Path::new(&filename);
+        if let Some(parent) = path.parent() {
+            std::fs::create_dir_all(parent)?;
         }
+        let response = client.get(&url).send()?;
+        let mut file = File::create(path)?;
+        file.write_all(&response.bytes()?)?;
     }
 
-    writeln!(cc_out, "}} // namespace httplib")?;
+    println!("Splitting httplib.h...");
 
-    println!("Wrote {} and {}", h_out_path, cc_out_path);
+    let in_file = "httplib.h";
+    let h_out = "vendor/cpp-httplib/httplib.h";
+    let cc_out = "vendor/cpp-httplib/httplib.cpp";
+
+    let do_split = if Path::new(h_out).exists() {
+        let in_time = std::fs::metadata(in_file)?.modified()?;
+        let out_time = std::fs::metadata(h_out)?.modified()?;
+        in_time > out_time
+    } else {
+        true
+    };
+
+    if do_split {
+        let border = "// ----------------------------------------------------------------------------";
+        let mut in_implementation = false;
+
+        std::fs::create_dir_all("vendor/cpp-httplib")?;
+
+        let file = File::open(in_file)?;
+        let reader = BufReader::new(file);
+
+        let mut fh = File::create(h_out)?;
+        let mut fc = File::create(cc_out)?;
+
+        writeln!(fc, "#include \"httplib.h\"")?;
+        writeln!(fc, "namespace httplib {{")?;
+
+        for line in reader.lines() {
+            let line = line?;
+            if line.contains(border) {
+                in_implementation = !in_implementation;
+            } else if in_implementation {
+                let replaced = line.replace("inline ", "");
+                writeln!(fc, "{}", replaced)?;
+            } else {
+                writeln!(fh, "{}", line)?;
+            }
+        }
+
+        writeln!(fc, "}} // namespace httplib")?;
+
+        println!("Wrote {} and {}", h_out, cc_out);
+    } else {
+        println!("{} and {} are up to date", h_out, cc_out);
+    }
+
+    std::fs::remove_file("httplib.h")?;
+
     Ok(())
 }
